67ea03f83b0bbf8010d2afb3c1bf1a9b25599ed4
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-09-19 22:58:18 -0400

    Add login/signup

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
-from flask import Flask, render_template, url_for
-from forms import RegistrationForm, LoginIn
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "dee056056241bb46c750468e771c98c2"
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
-@app.route("/dashboard")
+
+@app.route("/dashboard") 
 @app.route("/")
-def dashboard():
+def dashboard(): 
     render_template("dashboard.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
-    SignForm = RegistrationForm()
-    render_template("login.html", title="Sign Up", form=SignForm)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'sucess')
+        return redirect(url_for('home'))
+    render_template("signup.html", title="Sign Up", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    LoginForm = LoginIn()
-    render_template("signup.html", title="Login In", form=LoginForm)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsucessful. Please check username and password', 'danger')
+    return render_template("login.html", title="Login In", form=form)
 
 
 if __name__ == "__main__":
