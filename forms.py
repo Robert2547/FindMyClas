f93fe2c20c43f6376d14104496e08a5ba8a73997
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-09-19 23:07:22 -0400

    Revert "Add login/signup"
    This reverts commit 67ea03f83b0bbf8010d2afb3c1bf1a9b25599ed4.

--- forms.py
+++ forms.py
@@ -1,22 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    username = StringField(
+        "Username", validators=["DataRequired", "Length(min=2, max=20)"]
+    )
+    email = StringField("Email", validators=["DataRequired", "Email", "Length(max=50)"])
+    password = PasswordField(
+        "Password", validators=["DataRequired", "Length(min=6, max=20)"]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=["DataRequired", "EqualTo(password)"]
+    )
+    sumbit = SubmitField("Sign Up")
 
 
-class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+class LoginIn(FlaskForm):
+    email = StringField("Email", validators=["DataRequired", "Email", "Length(max=50)"])
+    password = PasswordField(
+        "Password", validators=["DataRequired", "Length(min=6, max=20)"]
+    )
+    remember = BooleanField("Remember Me")
+    sumbit = SubmitField("Login in")
