36872a392fdfd911b7cbf333c5452986bb78ec89
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-09-20 17:38:49 -0400

    Layout changes

--- main.py
+++ main.py
@@ -1,48 +1,38 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
-app.config["SECRET_KEY"] = "dee056056241bb46c750468e771c98c2"
+app.config["SECRET_KEY"] = "dee056056241bb46c750468e771c98c2" # secret key to prevent cross-site request forgery attacks
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
+db = SQLAlchemy(app) # database instance
 
 @app.route("/dashboard") 
 @app.route("/")
 def dashboard(): 
-    render_template("dashboard.html")
+    return render_template("dashboard.html")
 
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
+    # if form is valid, redirect to home page, else render signup page
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'sucess')
         return redirect(url_for('home'))
     render_template("signup.html", title="Sign Up", form=form)
 
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+    form = LoginForm() # create instance of LoginForm
+    if form.validate_on_submit(): # if form is valid
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password': # if email and password are correct
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('home')) # redirect to home page
         else:
             flash('Login Unsucessful. Please check username and password', 'danger')
     return render_template("login.html", title="Login In", form=form)
