21350b029323a979ac45d051e5814e4b03afc8ad
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-09-24 20:27:38 -0400

    signup form

--- main/routes.py
+++ main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from main import app
+from main import app, db, bcrypt
 from main.forms import SignForm, LoginForm
 from main.models import User, Course
 
@@ -14,8 +14,12 @@ def home():
 def signup(): # signup route
     form = SignForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to log in!', 'success')
+        return redirect(url_for('login'))
     return render_template('signup.html', title='Register', form=form)
 
 
