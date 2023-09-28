bf18374350603e1707bf3f00c45c0be1afa1b0bf
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-09-28 00:26:59 -0400

    Add popup, navbar

--- client/src/nav/Navbar.js
+++ client/src/nav/Navbar.js
@@ -1,30 +1,42 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Authnav from "./Authnav";
-import Nonauth from "./Nonauth";
+import React from 'react'
+import Popup from './Popup'
 
 function Navbar() {
-  //This function checks if the user is login or not
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  useEffect(() => {
-    const fetchAuthStatus = async () => {
-      try {
-        const response = await axios.get("/authorized"); //Make GET request to Flask Endpoint
-
-        setIsAuthenticated(response.data.authenticated); //Set state to response (True or False)
-      } catch (error) {
-        console.error("Error fetching authentication!"), error;
-      }
-    };
-
-    fetchAuthStatus();
-  }, []); //useEffect runs once when the component render
-
   return (
-    //If the user is authenticated, show the Authnav component, else show the Nonauth component
-    <div>{isAuthenticated ? <Authnav /> : <Nonauth />};</div>
-  );
-}
+    <div className="container-fluid">
+    <div className="row flex-nowrap">
+      <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+          <a href="{{ url_for('home') }}" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <span className="fs-5 d-none d-sm-inline">FindMyClass</span>
+          </a>
+          <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+            <li className="nav-item">
+              <a href="{{ url_for('home') }}" className="nav-link align-middle px-0">
+                <i className="fs-4 bi-house"></i>
+                <span className="ms-1 d-none d-sm-inline">Home</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="{{ url_for('login') }}" className="nav-link align-middle px-0">
+                <i className="fs-4 bi-house"></i>
+                <span className="ms-1 d-none d-sm-inline">Login</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="{{ url_for('signup') }}" className="nav-link align-middle px-0">
+                <i className="fs-4 bi-house"></i>
+                <span className="ms-1 d-none d-sm-inline">Signup</span>
+              </a>
+            </li>
+          </ul>
+          <hr />
+          <Popup />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+};
 
-export default Navbar;
+export default Navbar
---
+++ client/src/nav/Popup.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Authnav from "./Authnav";
+import Nonauth from "./Nonauth";
+
+function Popup() {
+  //This function checks if the user is login or not
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const fetchAuthStatus = async () => {
+      try {
+        const response = await axios.get("/authorized"); //Make GET request to Flask Endpoint
+
+        setIsAuthenticated(response.data.authenticated); //Set state to response (True or False)
+      } catch (error) {
+        console.error("Error fetching authentication!"), error;
+      }
+    };
+
+    fetchAuthStatus();
+  }, []); //useEffect runs once when the component render
+
+  return (
+    //If the user is authenticated, show the Authnav component, else show the Nonauth component
+    <div>{isAuthenticated ? <Authnav /> : <Nonauth />};</div>
+  );
+}
+  
+export default Popup;
