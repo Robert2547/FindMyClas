52db824ed432222784393777d859b44a014e79be
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2023-10-02 20:08:20 -0400

    Add fetch abort features

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,28 +1,41 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 function useForm() {
-    const [values, setValues] = useState(null);
-    const[pending, setPending] = useState(true);
-    const[errors, setErrors] = useState(null);
+  const [values, setValues] = useState(null);
+  const [pending, setPending] = useState(true);
+  const [errors, setErrors] = useState(null);
 
-    useEffect((url) => {
-        fetch(url)
-        .then(res => {
-            if(!res.ok) {
-                throw Error('Could not fetch data for that resource')
-            }
-            res.json()}).then(data => {
-                setValues(data);
-                setPending(false);
-                setErrors(null);
-            })
-            .catch(err => {
-                setPending(false);
-                setErrors(err.message);
-            })
-    }, [])
-    
-  return {values, pending, errors}
+  useEffect(
+    (url) => {
+      const abortFetch = new AbortController();
+      fetch(url, { signal: abortFetch.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch data for that resource");
+          }
+          res.json();
+        })
+        .then((data) => {
+          setValues(data);
+          setPending(false);
+          setErrors(null);
+        })
+        .catch((err) => {
+          // if the fetch is aborted, don't update state
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            // otherwise, update state
+            setPending(false);
+            setErrors(err.message);
+          }
+        });
+      return () => abortFetch.abort(); // abort the fetch
+    },
+    [url]
+  );
+
+  return { values, pending, errors };
 }
 
-export default useForm
+export default useForm;
